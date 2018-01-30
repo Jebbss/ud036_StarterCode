e5875262c7c1ca9f5fa9141d6dcb394d8c4842c7
Jebbss <jebsumerac@gmail.com>
2018-01-29 21:08:54 -0600

    PEP8

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,23 +10,23 @@ resvoir_dogs = media.Movie(
 pulp_fiction = media.Movie(
                             'Pulp Fiction', 'Four alinear wacky stories',
                             'https://www.youtube.com/watch?v=Mnb_3ibUp38',
-                            'https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg') # NOQA
+                            'https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg')  # NOQA
 jackie_brown = media.Movie(
                             'Jack Brown', 'A relationship evolves',
                             'https://www.youtube.com/watch?v=G7HkBDNZV7s',
-                            'https://upload.wikimedia.org/wikipedia/en/8/80/Jackie_Brown70%27s.jpg') # NOQA
+                            'https://upload.wikimedia.org/wikipedia/en/8/80/Jackie_Brown70%27s.jpg')  # NOQA
 kill_bill_one = media.Movie(
                             'Kill Bill Vol.1', 'A bride hunts her groom',
                             'https://www.youtube.com/watch?v=7kSuas6mRpk',
-                            'https://upload.wikimedia.org/wikipedia/en/2/2c/Kill_Bill_Volume_1.png') # NOQA
+                            'https://upload.wikimedia.org/wikipedia/en/2/2c/Kill_Bill_Volume_1.png')  # NOQA
 kill_bill_two = media.Movie(
                             'Kill Bill Vol.2', 'A bride finds her groom',
                             'https://www.youtube.com/watch?v=WTt8cCIvGYI',
-                            'https://upload.wikimedia.org/wikipedia/en/c/c4/Kill_Bill_Volume_2.png') # NOQA
+                            'https://upload.wikimedia.org/wikipedia/en/c/c4/Kill_Bill_Volume_2.png')  # NOQA
 inglourious = media.Movie(
                           'Inglourious', 'A mission does not go to plan',
                           'https://www.youtube.com/watch?v=KnrRy6kSFF0',
-                          'https://upload.wikimedia.org/wikipedia/en/c/c3/Inglourious_Basterds_poster.jpg') # NOQA
+                          'https://upload.wikimedia.org/wikipedia/en/c/c3/Inglourious_Basterds_poster.jpg')  # NOQA
 
 # Store movies in a list
 movies = [resvoir_dogs, pulp_fiction, jackie_brown,
